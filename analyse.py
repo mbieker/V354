7fec5af3dfa14df0172f92dbc6f4b2eb8610d287
Martin Bieker <martin.bieker@tu-dortmund.de>
2014-01-07 15:04:59 +0100

    Python Skript angefangen. Einlesen der CSV Files ist nun möglich

---
+++ analyse.py
@@ -0,0 +1,186 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  6 19:48:55 2014
+
+@author: martin
+"""
+
+import csv
+from numpy import *
+import matplotlib
+from uncertainties import  ufloat
+
+def load_scope(path, scale = True):
+    data = open(path,'rb')
+    time = []
+    voltage = []
+    reader = csv.reader(data, delimiter = ',')
+    for line in reader:
+        time.append(float(line[3]))
+        voltage.append(float(line[4]))
+    return array(time), array(voltage)
+    
+def peakdet(v, delta, x = None):
+    """
+    Converted from MATLAB script at http://billauer.co.il/peakdet.html
+    
+    Returns two arrays
+    
+    function [maxtab, mintab]=peakdet(v, delta, x)
+    %PEAKDET Detect peaks in a vector
+    %        [MAXTAB, MINTAB] = PEAKDET(V, DELTA) finds the local
+    %        maxima and minima ("peaks") in the vector V.
+    %        MAXTAB and MINTAB consists of two columns. Column 1
+    %        contains indices in V, and column 2 the found values.
+    %      
+    %        With [MAXTAB, MINTAB] = PEAKDET(V, DELTA, X) the indices
+    %        in MAXTAB and MINTAB are replaced with the corresponding
+    %        X-values.
+    %
+    %        A point is considered a maximum peak if it has the maximal
+    %        value, and was preceded (to the left) by a value lower by
+    %        DELTA.
+    
+    % Eli Billauer, 3.4.05 (Explicitly not copyrighted).
+    % This function is released to the public domain; Any use is allowed.
+    
+    """
+    maxtab = []
+    mintab = []
+       
+    if x is None:
+        x = arange(len(v))
+    
+    v = asarray(v)
+    
+    if len(v) != len(x):
+        sys.exit('Input vectors v and x must have same length')
+    
+    if not isscalar(delta):
+        sys.exit('Input argument delta must be a scalar')
+    
+    if delta <= 0:
+        sys.exit('Input argument delta must be positive')
+    
+    mn, mx = Inf, -Inf
+    mnpos, mxpos = NaN, NaN
+    
+    lookformax = True
+    
+    for i in arange(len(v)):
+        this = v[i]
+        if this > mx:
+            mx = this
+            mxpos = x[i]
+        if this < mn:
+            mn = this
+            mnpos = x[i]
+        
+        if lookformax:
+            if this < mx-delta:
+                maxtab.append((mxpos, mx))
+                mn = this
+                mnpos = x[i]
+                lookformax = False
+        else:
+            if this > mn+delta:
+                mintab.append((mnpos, mn))
+                mx = this
+                mxpos = x[i]
+                lookformax = True
+ 
+    return array(maxtab), array(mintab)
+    
+
+def make_LaTeX_table(data,header, flip= 'false', onedim = 'false'):
+    output = '\\begin{tabular}{'
+    #Get dimensions
+    if(onedim == 'true'):
+        if(flip == 'false'):
+        
+            data = array([[i] for i in data])
+        
+        else:
+            data = array([data])
+        
+
+    row_cnt, col_cnt = data.shape
+    header_cnt = len(header)
+    
+    if(header_cnt == col_cnt and flip== 'false'):
+        #Make Format
+        output += '|'
+        for i in range(col_cnt):
+            output += 'c|'
+        output += '}\n\\hline\n'+ header[0]
+        for i in range (1,col_cnt):
+            output += ' & ' + header[i]
+        output += ' \\\\\n\\hline\n'
+        for i in data:
+            output += str(i[0])
+            for j in range(1,col_cnt):
+                output += ' & ' + str( i[j])
+            output += '\\\\\n'
+        output += '\\hline\n\\end{tabular}\n'
+                            
+        return output
+    else:
+        if(row_cnt == header_cnt):
+            output += '|c|' + (col_cnt)*'c' + '|}\n\\hline\n'
+            for i in range(row_cnt):
+                output += header[i]
+                for j in range(col_cnt):
+                    output += ' & ' + str(data[i][j])
+                output += '\\\\\n\\hline\n'
+                
+            output += '\\end{tabular}\n'
+            return output
+        else:
+            return 'ERROR'
+
+    
+def lin_reg(x,y):
+    N = len(x)
+    sumx = x.sum()
+    sumy = y.sum()
+    sumxx = (x*x).sum()
+    sumxy = (x*y).sum()
+    m = (sumxy -  sumx*sumy/N)/(sumxx- sumx**2/N)
+    b = sumy/N - m*sumx/N
+    
+    sy = sqrt(((y - m*x - b)**2).sum()/(N-1))
+    m_err = sy *sqrt(N/(N*sumxx - sumx**2))
+    b_err= m_err * sqrt(sumxx/N)
+    return ufloat(m,m_err), ufloat(b,b_err)
+    
+    
+###### ANFANG DER ANALYSE ##################################################
+time, voltage = load_scope('./Messwerte/ALL0000/F0000CH1.CSV')
+
+voltage = voltage -10.2 #offset aus CSV File
+max_ , min_ = peakdet(voltage, 1, time )
+
+tmp =list(max_)
+tmp.pop(0)
+max_= array(tmp).T
+min_ = min_.T
+
+# Erste Grafik erzeugen
+close()
+plot(time, voltage , 'k-')
+plot(max_[0],max_[1] ,'ko', markersize=5)
+plot(min_[0],min_[1] ,'ko',  markersize=5)
+show()
+
+# extrema zu einer liste hinzufügen und minima spigeln
+
+
+
+regression_times = max_[0]
+regression_voltages =max_[1]
+print(lin_reg(regression_times,log(regression_voltages)))
+
+close()
+plot(regression_times,log(regression_voltages), 'x')
+
+show()
